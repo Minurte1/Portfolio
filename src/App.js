6ef59ef9cfb5a1ccbddff7f3a1ae99843fc3e2cb
Minurte1 <hohoangphucjob@gmail.com>
2024-02-10 21:41:02 +0700

    cap nhat

--- src/App.js
+++ src/App.js
@@ -352,6 +352,7 @@ const App = () => {
             </Col>
             <Col md={6}>
               <Card className='Education-2'>
+                <span className='Education-1-time'>2005 - 2009</span>
                 <Card.Title className='Education-1-Text'>ABOUT ME</Card.Title>
                 <Card.Text className='Education-1-Text1'>
                   Transforming visions into
